cd5d3097347854e7af8837a891bd794322a7afbb
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-05-26 16:22:32 +0300

    Changed sign-out button color

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -52,7 +52,7 @@ export default async function AuthButton() {
     <div className="flex items-center gap-4">
       Hey, {user.email}!
       <form action={signOutAction}>
-        <Button type="submit" variant={"outline"}>
+        <Button type="submit" variant={"outline"} className=" bg-red-800 hover:bg-red-500">
           Sign out
         </Button>
         
