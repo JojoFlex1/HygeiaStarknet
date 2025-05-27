6e2d574c1039ff6978b3c8b57ad5315bb44b07c7
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-05-27 20:22:48 +0300

    Changed product prices

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -14,19 +14,19 @@ export default function HomePage() {
            image="/images/menscup.png"
            title="Menstrual Cup - Medium Size"
            description="Medical-grade silicone menstrual cup that offers up to 12 hours of protection. Reusable, eco-friendly, and cost-effective"
-           price={4500}
+           price={35.00}
           />
           <ProductCard
            image="/images/organicPad.png"
            title="Eco-friendly Organic Pads"
            description="100% organic cotton pads that are biodegradable and free from plastics, perfumes, and chemicals. Comfortable and perfect for daily use, sensitive skin, and an eco-conscious lifestyle."
-           price={999}
+           price={0.55}
           />
           <ProductCard
            image="/images/periodPanty.png"
            title="Period Panties - Hipster Style"
            description="Absorbent, leak-proof underwear that replaces disposable pads and tampons. Can hold up to 2 tampons worth of flow. Washable, reusable, and designed for comfort and confidence all day long."
-           price={5200}
+           price={20.00}
           />
         </div>
   )
