03eab45daa4cdf712dee5c70b5d462b73658a320
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-06-03 14:35:19 +0300

    Modified App's Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,8 +63,8 @@ export const metadata = {
 
   icons: {
     icon: '/images/logo.png',
-    // shortcut: '/favicon.ico',
-    // apple: '/apple-touch-icon.png', // optional
+    shortcut: '/images/logo.png',
+    apple: '/images/logo.png', // optional
   },
 
   alternates: {
