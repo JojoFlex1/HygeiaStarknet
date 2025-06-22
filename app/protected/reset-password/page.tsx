ad5817be7c5993ebea93d924fbaae67a32072058
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-06-22 19:51:29 +0300

    Removed unnecessary directories

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -35,3 +35,27 @@ export default async function ResetPassword(props: {
     </form>
   );
 }
+
+//BELOW IS THE protected/ page.tsx FILE INCASE YOU NEED IT IN THE FUTURE
+
+// import { createClient } from "@/utils/supabase/server";
+// import { InfoIcon } from "lucide-react";
+// import { redirect } from "next/navigation";
+// import HomePage from "../home/page";
+
+// export default async function ProtectedPage() {
+//   const supabase = await createClient();
+
+//   const {
+//     data: { user },
+//   } = await supabase.auth.getUser();
+
+//   if (!user) {
+//     return redirect("/sign-in");
+//   }
+//   return (
+//     <div className=" flex-1">
+//       <HomePage/>
+//     </div>
+//   );
+// }
